beaa73a7d4b2802a22f208ed027193a8acc09c84
Harper Foley <harper.e.foley@gmail.com>
2022-12-20 18:54:22 -0800

    Adds 10 tests for Fib and Lucas series All 10 tests pass.

--- modules/series.py
+++ modules/series.py
@@ -1,3 +1,14 @@
 # Return nth number in Fibonacci sequence
 
 def fibonacci(n):
+    a, b = 0, 1
+    while a < n:
+        a, b = b, a + b
+    return a
+
+
+def lucas(n):
+    prev, curr = 2, 1
+    for _ in range(n):
+        prev, curr = curr, prev+curr
+    return prev
--- tests/test_series.py
+++ tests/test_series.py
@@ -1,14 +1,53 @@
-import pytest
-from series import fibonacci, lucas
+from modules.series import fibonacci, lucas
 
 
-def test_fibonacci(n):
+def test_fibonacci_0():
+    actual = fibonacci(0)
+    expected = 0
+    assert actual == expected
+
+def test_fibonacci_5():
     actual = fibonacci(5)
-    expected = "5"
-    actual == expected
+    expected = 5
+    assert actual == expected
+
+def test_fibonacci_1():
+    actual = fibonacci(1)
+    expected = 1
+    assert actual == expected
 
 
-def test_lucas(n):
+def test_lucas_0():
+    actual = lucas(0)
+    expected = 2
+    assert actual == expected
+
+def test_lucas_1():
+    actual = lucas(1)
+    expected = 1
+    assert actual == expected
+
+def test_lucas_2():
+    actual = lucas(2)
+    expected = 3
+    assert actual == expected
+
+def test_lucas_3():
+    actual = lucas(3)
+    expected = 4
+    assert actual == expected
+
+def test_lucas_4():
+    actual = lucas(4)
+    expected = 7
+    assert actual == expected
+
+def test_lucas_5():
     actual = lucas(5)
-    expected = "11"
+    expected = 11
+    assert actual == expected
+
+def test_lucas_6():
+    actual = lucas(6)
+    expected = 18
     assert actual == expected
